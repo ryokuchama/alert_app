bf7d1c51d065ce63643fbc0e9dda9f2cb98b6959
ryokuchama <r.madrid.pg@gmail.com>
2023-03-03 20:14:06 +0900

    first commit

---
+++ Alert/Alert/AlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertApp.swift
+//  Alert
+//
+//  Created by 梶健将 on 2023/03/03.
+//
+
+import SwiftUI
+
+@main
+struct AlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
